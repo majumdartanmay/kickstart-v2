8a7468651c4e075d49aa8d12634f3d13c5dc00f3
majumdartanmay <tanmay.majumdar@hotmail.com>
2026-03-05 10:52:22 +0530

    Add more harpoon options

--- lua/user.lua
+++ lua/user.lua
@@ -6,12 +6,12 @@ vim.keymap.set(
   function() require('telescope.builtin').find_files { hidden = true, no_ignore = true } end,
   { noremap = true, silent = false, desc = 'Telescope find files' }
 )
-vim.keymap.set(
-  'n',
-  '<leader>f3',
-  function() require('telescope.builtin').buffers { hidden = true, no_ignore = true } end,
-  { noremap = true, silent = false, desc = 'Telescope find buffers' }
-)
+-- vim.keymap.set(
+--   'n',
+--   '<leader>f3',
+--   function() require('telescope.builtin').buffers { hidden = true, no_ignore = true } end,
+--   { noremap = true, silent = false, desc = 'Telescope find buffers' }
+-- )
 vim.keymap.set(
   'n',
   '<leader>f4',
@@ -121,4 +121,24 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "3a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+--
+-- basic telescope harpoon configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<leader>f3", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
